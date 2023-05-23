ee5a5ddcf97851a8e86ce3194418ea1e2fb1fe49
juliauzbemb <90088899+juliauzbemb@users.noreply.github.com>
2023-05-24 01:03:22 +0300

    test

--- e2e/form.test.js
+++ e2e/form.test.js
@@ -6,7 +6,7 @@ describe('Testing form widget', () => {
 
   beforeEach(async () => {
     browser = await puppeteer.launch({
-      headless: true,
+      headless: "new",
       slowMo: 100,
       devtools: true,
     });
--- e2e/start.test.js
+++ e2e/start.test.js
@@ -6,7 +6,7 @@ describe('Page start', () => {
 
   beforeEach(async () => {
     browser = await puppeteer.launch({
-      headless: true,
+      headless: "new",
       slowMo: 100,
       devtools: true,
     });
